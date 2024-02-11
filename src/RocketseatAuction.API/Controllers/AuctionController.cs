8a2e55a09613d7b812d3794c1d9b85232c6b32ef
Matheus <ymatheus.h@gmail.com>
2024-02-10 21:44:23 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction()
+    {
+        return Ok("Hello World");
+    }
+}

6ea55bcc8599dc651ff85c9e378b0e599e9dcab5
Matheus <ymatheus.h@gmail.com>
2024-02-11 12:40:01 -0300

    refactor: implements repository dependency injection

--- src/RocketseatAuction.API/Program.cs
+++ src/RocketseatAuction.API/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.Repositories;
+using RocketseatAuction.API.Repositories.DataAccess;
 using RocketseatAuction.API.Services;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 using RocketseatAuction.API.UseCases.Offers.CreateOffer;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -45,6 +50,15 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddScoped<AuthenticationUserAttribute>();
 builder.Services.AddScoped<LoggedUser>();
 builder.Services.AddScoped<CreateOfferUseCase>();
+builder.Services.AddScoped<GetCurrentAuctionUseCase>();
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+builder.Services.AddScoped<IOfferRepository, OfferRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddDbContext<RocketseatAuctionDbContext>(options =>
+{
+    options.UseSqlite("Data Source=C:\\Users\\ymath\\source\\repos\\RocketseatAuction\\leilaoDbNLW.db");
+});
 
 builder.Services.AddHttpContextAccessor();
 
--- src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -1,21 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Entities;
-using RocketseatAuction.API.Repositories;
 
 namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 public class GetCurrentAuctionUseCase
 {
+    private readonly IAuctionRepository _repository;
+
+    public GetCurrentAuctionUseCase(IAuctionRepository repository) => _repository = repository;
 
     public Auction? Execute()
     {
-        var repository = new RocketseatAuctionDbContext();
-
-        var today = DateTime.Now;
-
-        return repository
-            .Auctions
-            .Include(auction => auction.Items)
-            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+        return _repository.GetCurrent();
     }
 }

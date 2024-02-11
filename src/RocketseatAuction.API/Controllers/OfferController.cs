1621b6ee0c13ad74b184e99c97ba48c3eb309743
Matheus <ymatheus.h@gmail.com>
2024-02-11 00:01:17 -0300

    feat: implements offer create

---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+
+namespace RocketseatAuction.API.Controllers;
+
+[ServiceFilter(typeof(AuthenticationUserAttribute))]
+public class OfferController : RocketseatBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    public IActionResult CreateOffer(
+        [FromRoute] int itemId,
+        [FromBody] RequestCreateOffer request,
+        [FromServices] CreateOfferUseCase useCase)
+    {
+        var id = useCase.Execute(itemId, request);
+
+        return Created(string.Empty, id);
+    }
+}
